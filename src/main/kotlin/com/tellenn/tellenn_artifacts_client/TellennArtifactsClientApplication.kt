8cb3d11ed9b54d0d0bc4ec6ddc73c8325c759e47
Kyllian <kyllian.perrin@inovatel.com>
2025-06-10 13:30:37 +0200

    Init the kotlin project

---
+++ src/main/kotlin/com/tellenn/tellenn_artifacts_client/TellennArtifactsClientApplication.kt
@@ -0,0 +1,11 @@
+package com.tellenn.tellenn_artifacts_client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TellennArtifactsClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<TellennArtifactsClientApplication>(*args)
+}
---
+++ src/test/kotlin/com/tellenn/tellenn_artifacts_client/TellennArtifactsClientApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tellenn.tellenn_artifacts_client
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TellennArtifactsClientApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
