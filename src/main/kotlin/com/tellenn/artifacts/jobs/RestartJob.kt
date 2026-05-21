a9ae6b444cc8a4a7cfcf2808499e95cc43877ca4
Kyllian <kyllian.perrin@inovatel.com>
2026-05-21 09:59:19 +0200

    Update the restart logic to a thread level (to iterate on to completely remove it)

--- src/main/kotlin/com/tellenn/artifacts/jobs/RestartJob.kt
+++ src/main/kotlin/com/tellenn/artifacts/jobs/RestartJob.kt
@@ -1,24 +1,26 @@
 package com.tellenn.artifacts.jobs
 
+import com.tellenn.artifacts.services.ThreadService
+import com.tellenn.artifacts.services.sync.CharacterSyncService
 import org.apache.logging.log4j.LogManager
-import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
-/**
- * Job to restart the application daily at 00:00.
- */
 @Component
-class RestartJob(private val context: ConfigurableApplicationContext) {
+class RestartJob(private val threadService: ThreadService, private val characterSyncService: CharacterSyncService) {
     private val log = LogManager.getLogger(RestartJob::class.java)
 
     /**
-     * Shuts down the application at 00:00 every day.
-     * When running in Docker with a restart policy, this will trigger a restart.
+     * Restart every character thread, removing "blackages" if needed
      */
     @Scheduled(cron = "0 0 0 * * *")
     fun restartApplication() {
+
         log.info("Daily restart scheduled: Shutting down application context at 00:00")
-        context.close()
+        threadService.stopAllThreads()
+
+        characterSyncService.syncPredefinedCharacters().map { (conf,_) ->
+            threadService.startCharacterThread(conf)
+        }
     }
 }
